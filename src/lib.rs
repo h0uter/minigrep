1361947d0eb6dd2dd37855fb66a35aa844007db2
W. Meijer <wouter.meijer@tno.nl>
2024-07-23 23:28:31 +0200

    remove commented code

--- src/lib.rs
+++ src/lib.rs
@@ -46,20 +46,6 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    // let mut results = Vec::new();
-
-    // for line in contents.lines() {
-    //     if line.contains(query) {
-    //         results.push(line)
-    //     }
-    // }
-
-    // results
-
-    //
-    // can be replaced by:
-    //
-
     contents
         .lines()
         .filter(|line| line.contains(query))
